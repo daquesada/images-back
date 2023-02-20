2f64abab6d0ae2acfc888de7c0b6628888dc6580
daquesada <daquesada17@gmail.com>
2023-02-20 09:42:59 -0500

    [FEATURE] add port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`server running on port ${Number(port)}`);
